7e8599520bdb817c4439bad9252ad3d122723175
Gerd Zschaler <gerd@gzschaler.de>
2018-02-14 20:10:47 +0100

    Remove debug code

--- main.go
+++ main.go
@@ -12,11 +12,4 @@ func main() {
 			return netcup.Provider()
 		},
 	})
-	//client := api.Client{os.Getenv("NETCUP_WS_USER"), os.Getenv("NETCUP_WS_PASSWORD")}
-	//ips, err := client.GetVServerIPs("v22018025690561161")
-	//if err != nil {
-	//	fmt.Printf("Fatal %s\n", err)
-	//	return
-	//}
-	//fmt.Printf("Got IPs %s\n", ips)
 }
